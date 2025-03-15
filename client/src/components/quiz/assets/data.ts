e82445842d8f954b0f2d3dabae9110cb03820e52
DShreyas <daya74b@gmail.com>
2025-03-15 20:33:02 +0530

    s

--- client/src/components/quiz/assets/data.ts
+++ client/src/components/quiz/assets/data.ts
@@ -79,5 +79,87 @@ export const data=[
       option4: "Home Tool Markup Language",
       ans: 1
     },
+    
+    {
+        "question": "Which language is used to build the structure of a web page?",
+        "option1": "HTML",
+        "option2": "CSS",
+        "option3": "JavaScript",
+        "option4": "Python",
+        "ans": 1
+      },
+      {
+        "question": "Which technology is used to style web pages in the MERN stack?",
+        "option1": "HTML",
+        "option2": "CSS",
+        "option3": "React",
+        "option4": "MongoDB",
+        "ans": 2
+      },
+      {
+        "question": "What does the 'M' in MERN stack stand for?",
+        "option1": "MongoDB",
+        "option2": "MySQL",
+        "option3": "Mongoose",
+        "option4": "Machine Learning",
+        "ans": 1
+      },
+      {
+        "question": "Which library is used for building user interfaces in the MERN stack?",
+        "option1": "Angular",
+        "option2": "Vue.js",
+        "option3": "React",
+        "option4": "jQuery",
+        "ans": 3
+      },
+      {
+        "question": "Which backend framework is used in the MERN stack?",
+        "option1": "Django",
+        "option2": "Flask",
+        "option3": "Express.js",
+        "option4": "Laravel",
+        "ans": 3
+      },
+      {
+        "question": "Which language is primarily used in the MERN stack?",
+        "option1": "Python",
+        "option2": "JavaScript",
+        "option3": "Java",
+        "option4": "C++",
+        "ans": 2
+      },
+      {
+        "question": "Which database is used in the MERN stack?",
+        "option1": "MySQL",
+        "option2": "MongoDB",
+        "option3": "PostgreSQL",
+        "option4": "SQLite",
+        "ans": 2
+      },
+      {
+        "question": "Which of the following is a package manager for Node.js?",
+        "option1": "NPM",
+        "option2": "PIP",
+        "option3": "Composer",
+        "option4": "Yarn",
+        "ans": 1
+      },
+      {
+        "question": "What does JSX stand for in React?",
+        "option1": "JavaScript XML",
+        "option2": "JavaScript Extension",
+        "option3": "Java XML",
+        "option4": "JSON XML",
+        "ans": 1
+      },
+      {
+        "question": "Which command is used to start a Node.js server?",
+        "option1": "node server.js",
+        "option2": "python server.py",
+        "option3": "npm run python",
+        "option4": "npm start",
+        "ans": 4
+      },
+    
   ];
   
