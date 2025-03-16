1785ac1c9dd3b96fe4e84120c1f980f83bac7fc5
BhargavDevi <1si23is012@sit.ac.in>
2025-03-16 08:17:19 +0530

    s

--- client/src/components/quiz/assets/data.ts
+++ client/src/components/quiz/assets/data.ts
@@ -1,101 +1,101 @@
 export const data=[
-    {
-      question: "Which language is used to style web pages?",
-      option1: "HTML",
-      option2: "CSS",
-      option3: "JavaScript",
-      option4: "Python",
-      ans: 2
-    },
-    {
-      question: "What does HTML stand for?",
-      option1: "HyperText Markup Language",
-      option2: "High Tech Modern Language",
-      option3: "Hyperlink and Text Management Language",
-      option4: "Home Tool Markup Language",
-      ans: 1
-    },
     // {
-    //   question: "Which tag is used to create a hyperlink in HTML?",
-    //   option1: "<p>",
-    //   option2: "<a>",
-    //   option3: "<h1>",
-    //   option4: "<link>",
+    //   question: "Which language is used to style web pages?",
+    //   option1: "HTML",
+    //   option2: "CSS",
+    //   option3: "JavaScript",
+    //   option4: "Python",
     //   ans: 2
     // },
     // {
-    //   question: "Which of the following is a JavaScript framework?",
-    //   option1: "Django",
-    //   option2: "Laravel",
-    //   option3: "React",
-    //   option4: "Flask",
-    //   ans: 3
-    // },
-    // {
-    //   question: "Which of the following is not a programming language?",
-    //   option1: "Java",
-    //   option2: "Python",
-    //   option3: "HTML",
-    //   option4: "C++",
-    //   ans: 3
-    // },
-    // {
-    //   question: "What is the correct file extension for JavaScript files?",
-    //   option1: ".js",
-    //   option2: ".java",
-    //   option3: ".script",
-    //   option4: ".css",
-    //   ans: 1
-    // },
-    // {
-    //   question: "Which protocol is used to transfer web pages?",
-    //   option1: "FTP",
-    //   option2: "HTTP",
-    //   option3: "SMTP",
-    //   option4: "SSH",
-    //   ans: 2
-    // },
-    // {
-    //   question: "Which of the following is used to create responsive web designs?",
-    //   option1: "Bootstrap",
-    //   option2: "jQuery",
-    //   option3: "Node.js",
-    //   option4: "PHP",
-    //   ans: 1
-    // },
-    // {
-    //   question: "Which tag is used to insert an image in HTML?",
-    //   option1: "<img>",
-    //   option2: "<picture>",
-    //   option3: "<image>",
-    //   option4: "<src>",
-    //   ans: 1
-    // },
-    // {
-    //   question: "What does CSS stand for?",
+    //   question: "What does HTML stand for?",
     //   option1: "HyperText Markup Language",
     //   option2: "High Tech Modern Language",
-    //   option3: "cascading style sheet",
+    //   option3: "Hyperlink and Text Management Language",
     //   option4: "Home Tool Markup Language",
     //   ans: 1
     // },
+    {
+      question: "Which tag is used to create a hyperlink in HTML?",
+      option1: "<p>",
+      option2: "<a>",
+      option3: "<h1>",
+      option4: "<link>",
+      ans: 2
+    },
+    {
+      question: "Which of the following is a JavaScript framework?",
+      option1: "Django",
+      option2: "Laravel",
+      option3: "React",
+      option4: "Flask",
+      ans: 3
+    },
+    {
+      question: "Which of the following is not a programming language?",
+      option1: "Java",
+      option2: "Python",
+      option3: "HTML",
+      option4: "C++",
+      ans: 3
+    },
+    {
+      question: "What is the correct file extension for JavaScript files?",
+      option1: ".js",
+      option2: ".java",
+      option3: ".script",
+      option4: ".css",
+      ans: 1
+    },
+    {
+      question: "Which protocol is used to transfer web pages?",
+      option1: "FTP",
+      option2: "HTTP",
+      option3: "SMTP",
+      option4: "SSH",
+      ans: 2
+    },
+    {
+      question: "Which of the following is used to create responsive web designs?",
+      option1: "Bootstrap",
+      option2: "jQuery",
+      option3: "Node.js",
+      option4: "PHP",
+      ans: 1
+    },
+    {
+      question: "Which tag is used to insert an image in HTML?",
+      option1: "<img>",
+      option2: "<picture>",
+      option3: "<image>",
+      option4: "<src>",
+      ans: 1
+    },
+    {
+      question: "What does CSS stand for?",
+      option1: "HyperText Markup Language",
+      option2: "High Tech Modern Language",
+      option3: "cascading style sheet",
+      option4: "Home Tool Markup Language",
+      ans: 1
+    },
     
-    // {
-    //     "question": "Which language is used to build the structure of a web page?",
-    //     "option1": "HTML",
-    //     "option2": "CSS",
-    //     "option3": "JavaScript",
-    //     "option4": "Python",
-    //     "ans": 1
-    //   },
-    //   {
-    //     "question": "Which technology is used to style web pages in the MERN stack?",
-    //     "option1": "HTML",
-    //     "option2": "CSS",
-    //     "option3": "React",
-    //     "option4": "MongoDB",
-    //     "ans": 2
-    //   },
+    {
+        "question": "Which language is used to build the structure of a web page?",
+        "option1": "HTML",
+        "option2": "CSS",
+        "option3": "JavaScript",
+        "option4": "Python",
+        "ans": 1
+      },
+      {
+        "question": "Which technology is used to style web pages in the MERN stack?",
+        "option1": "HTML",
+        "option2": "CSS",
+        "option3": "React",
+        "option4": "MongoDB",
+        "ans": 2
+      },
     //   {
     //     "question": "What does the 'M' in MERN stack stand for?",
     //     "option1": "MongoDB",
